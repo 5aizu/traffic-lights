34af6e9472efac7b32eb675b4be1bbe8516f7d76
Saizu <111767336+5aizu@users.noreply.github.com>
2022-11-03 11:23:28 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let item = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+basic.forever(function () {
+    item.showColor(neopixel.colors(NeoPixelColors.Green))
+    for (let index = 0; index <= 20; index++) {
+        whaleysans.showNumber(Math.map(index, 1, 20, 20, 0))
+        basic.pause(1000)
+    }
+    item.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    for (let index = 0; index <= 3; index++) {
+        whaleysans.showNumber(Math.map(index, 1, 3, 3, 1))
+        basic.pause(1000)
+    }
+    item.showColor(neopixel.colors(NeoPixelColors.Red))
+    for (let index = 0; index <= 20; index++) {
+        whaleysans.showNumber(Math.map(index, 1, 20, 20, 0))
+        basic.pause(1000)
+    }
+})
