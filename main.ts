45f9e65b098b4d780f62352b7dd8474668127dcb
Saizu <111767336+5aizu@users.noreply.github.com>
2022-11-09 16:14:23 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let item = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
 basic.forever(function () {
     item.showColor(neopixel.colors(NeoPixelColors.Green))
     for (let index = 0; index <= 20; index++) {
-        whaleysans.showNumber(Math.map(index, 1, 20, 20, 0))
+        whaleysans.showNumber(Math.map(index, 1, 20, 20, 1))
         basic.pause(1000)
     }
     item.showColor(neopixel.colors(NeoPixelColors.Yellow))
@@ -12,7 +12,7 @@ basic.forever(function () {
     }
     item.showColor(neopixel.colors(NeoPixelColors.Red))
     for (let index = 0; index <= 20; index++) {
-        whaleysans.showNumber(Math.map(index, 1, 20, 20, 0))
+        whaleysans.showNumber(Math.map(index, 1, 20, 20, 1))
         basic.pause(1000)
     }
 })
